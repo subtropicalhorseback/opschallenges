2ee7008285c52de121cbb55737e84d8692360a63
Ian Bennett <subtropicalhorseback@gmail.com>
2023-10-24 14:31:50 -0500

    Completed Ops Challenge 02

---
+++ helloworld.sh
@@ -0,0 +1,28 @@
+# text marked as text doesn't matter!
+
+#!/bin/bash
+
+# Script Name: OC 02 Hello World
+# Author: Ian
+# Date of Latest Revision: 24/10/2023
+# Purpose: Class 02
+
+
+
+# Declaration of variables
+
+#   none
+
+# Declaration of functions
+
+#   none
+
+
+
+# Main
+
+echo "I am working perfectly!"
+
+
+
+# End
