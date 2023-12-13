fb8582a5c8703ba00842fa0b6e014d3ba69afdee
Ian Bennett <subtropicalhorseback@gmail.com>
2023-12-13 11:53:34 -0600

    completed 30113

--- 30113.ps1
+++ 30113.ps1
@@ -5,10 +5,10 @@
 
 
 Import-Module ActiveDirectory
-New-ADOrganizationalUnit -Name "ExampleOU" -Path "DC=globexpower,DC=com"
+New-ADOrganizationalUnit -Name "ExampleOU" -Path "DC=corp,DC=globexpower,DC=com"
 
-New-ADGroup -Name "ExampleGroup" -SamAccountName "ExampleGroup" -GroupScope Global -GroupCategory Security -Path "OU=ExampleOU,DC=globexpower,DC=com"
+New-ADGroup -Name "ExampleGroup" -SamAccountName "ExampleGroup" -GroupScope Global -GroupCategory Security -Path "OU=ExampleOU,DC=corp,DC=globexpower,DC=com"
 
-New-ADUser -Name "Franz Ferdinand" -SamAccountName "ferdif" -UserPrincipalName "ferdif@globexpower.com" -GivenName "Franz" -Surname "Ferdinand" -DisplayName "Franz Ferdinand" -Path "OU=ExampleOU,DC=globexpower,DC=com" -AccountPassword (ConvertTo-SecureString -AsPlainText "Solarwinds123" -Force) -Enabled $true
+New-ADUser -Name "Franz Ferdinand" -SamAccountName "ferdif" -UserPrincipalName "ferdif@globexpower.com" -GivenName "Franz" -Surname "Ferdinand" -DisplayName "Franz Ferdinand" -Path "OU=ExampleOU,DC=corp,DC=globexpower,DC=com" -AccountPassword (ConvertTo-SecureString -AsPlainText "Solarwinds123" -Force) -Enabled $true
 
 Add-ADGroupMember -Identity "ExampleGroup" -Members "ferdif"
