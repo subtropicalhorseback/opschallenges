02e1c50247bbed4ac837848d41cc96ae3f1013e9
Ian Bennett <subtropicalhorseback@gmail.com>
2023-12-13 11:28:55 -0600

    30113 draft 1

---
+++ 30113.ps1
@@ -0,0 +1,14 @@
+
+# https://www.manageengine.com/products/ad-manager/powershell/create-new-adorganizationalunit-using-powershell.html
+
+# https://github.com/codefellows/seattle-ops-301d14/tree/main/class-13/challenges
+
+
+Import-Module ActiveDirectory
+New-ADOrganizationalUnit -Name "ExampleOU" -Path "DC=globexpower,DC=com"
+
+New-ADGroup -Name "ExampleGroup" -SamAccountName "ExampleGroup" -GroupScope Global -GroupCategory Security -Path "OU=ExampleOU,DC=globexpower,DC=com"
+
+New-ADUser -Name "Franz Ferdinand" -SamAccountName "ferdif" -UserPrincipalName "ferdif@globexpower.com" -GivenName "Franz" -Surname "Ferdinand" -DisplayName "Franz Ferdinand" -Path "OU=ExampleOU,DC=globexpower,DC=com" -AccountPassword (ConvertTo-SecureString -AsPlainText "Solarwinds123" -Force) -Enabled $true
+
+Add-ADGroupMember -Identity "ExampleGroup" -Members "ferdif"
