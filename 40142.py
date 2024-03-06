b1dbefa00d8436e61c9de1006bbb496827d52302
Ian Bennett <subtropicalhorseback@gmail.com>
2024-03-05 22:01:02 -0600

    40142 final

--- 40142.py
+++ 40142.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 # demo code from https://github.com/codefellows/seattle-cybersecurity-401d10/blob/main/class-42/challenges/DEMO.md
+
+
+# import
 import nmap
 
+# use the scanner throughout
 scanner = nmap.PortScanner()
 
+# header
 print("Nmap Automation Tool")
 print("--------------------")
 
+# ip intake and print
 ip_addr = input("IP address to scan: ")
 print("The IP you entered is: ", ip_addr)
 
-# Prompting the user for the scan type including the third scan type option.
+# choose options
 resp = input("""\nSelect scan to execute:
                 1) SYN ACK Scan
                 2) UDP Scan
-                3) Comprehensive Scan\n""") # Completed the third scan option
+                3) Comprehensive Scan\n""") 
 print("You have selected option: ", resp)
 
-# Prompt the user to type in a port range for the scan.
-range = input("Enter port range to scan (e.g., 1-1024): ") # Addressed the TODO for port range input
+# choose port range
+range = input("Enter port range to scan (e.g., 1-1024): ") 
 
+# option 1 - syn/ack
 if resp == '1':
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, range, '-v -sS')
@@ -27,8 +34,9 @@ if resp == '1':
     print("Ip Status: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+
+# option 2 - udp
 elif resp == '2':
-    # Added the missing code block for UDP Scan.
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, range, '-v -sU')
     print(scanner.scaninfo())
@@ -38,13 +46,16 @@ elif resp == '2':
         print("Open Ports: ", scanner[ip_addr]['udp'].keys())
     else:
         print("No UDP ports found.")
+
+# option 3 - comprehensive scan
 elif resp == '3':
-    # Added the missing code block for Comprehensive Scan.
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, range, '-v -sS -sU -sC -A -O')
     print(scanner.scaninfo())
     print("Ip Status: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("Open Ports: ", scanner[ip_addr]['tcp'].keys(), scanner[ip_addr].get('udp', {}).keys())
+
+# weak input handling
 else:
     print("Please enter a valid option")
